9ae014d1d499673e7ca8dcef5fbe9a3c6ec11d4c
julietruong <juliemytruong@gmail.com>
2024-07-19 14:54:08 +1000

    d

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,10 +14,23 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- #
 
 def start_timer():
+  count_down(5*60)
+
+
+
+def restart_timer():
   return()
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+  display=math.floor(count/60)
+  display_sec=count%60
+  canvas.itemconfig(timer_text,text=f"{display}:{display_sec}")
+  if count>0:
+    window.after(1000,count_down,count-1)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window= Tk()
@@ -26,7 +40,7 @@ window.config(padx=100,pady=50,bg=YELLOW)
 canvas=Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 tomato_img=PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+timer_text=canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
 canvas.grid(row=2,column=2)
 
 labelA = Label(text="Timer",font=(FONT_NAME,50,"bold"))
@@ -37,9 +51,10 @@ labelB = Label(text="✔",font=(FONT_NAME,20,"bold"))
 labelB.config(bg=YELLOW,fg=GREEN)
 labelB.grid(column=2, row=5)
 
-button_start = Button(text="start", command=start_timer)
-button_start.grid(column=1,row=5)
+button_start = Button(text="start", highlightthickness=0,command=start_timer)
+button_start.grid(column=1,row=4)
 
-
+button_end = Button(text="reset", command=restart_timer,highlightthickness=0)
+button_end.grid(column=3,row=4)
 
 window.mainloop()
